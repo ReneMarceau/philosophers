4d26bf15f4c6e984d78bc8f2da978e7c2fbf6aa4
ReneMarceau <rene.marceau.dev@gmail.com>
2023-06-26 21:28:03 -0400

    Die after max 10ms done

--- src/end_program.c
+++ src/end_program.c
@@ -6,7 +6,7 @@
 /*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 16:46:23 by rmarceau          #+#    #+#             */
-/*   Updated: 2023/06/26 18:16:39 by rmarceau         ###   ########.fr       */
+/*   Updated: 2023/06/26 21:19:17 by rmarceau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,23 @@
 // than the time allowed to pass without eating, else it returns false.
 bool	death_watcher(t_philo *philo, u_int64_t now)
 {
+	u_int64_t	time_to_die;
+	u_int64_t	time_to_eat;
+	u_int64_t	time_to_sleep;
+
+	time_to_die = philo->table->input.time_to_die;
+	time_to_eat = philo->table->input.time_to_eat;
+	time_to_sleep = philo->table->input.time_to_sleep;
 	pthread_mutex_lock(&philo->table->mutex[DEATH]);
-	if (now - philo->last_meal > philo->table->input.time_to_die)
+	if (now - philo->last_meal > time_to_die && !philo->table->stop)
 	{
+		philo->table->stop = true;
+		if ((time_to_die < time_to_eat) || (time_to_die < (time_to_eat
+					+ time_to_sleep)))
+			printf("%s%ld %ld %s%s", RED, time_to_die, philo->id, DIE, RESET);
+		else
+			printf("%s%ld %ld %s%s", RED, get_time() - philo->table->start_time,
+				philo->id, DIE, RESET);
 		pthread_mutex_unlock(&philo->table->mutex[DEATH]);
 		return (true);
 	}
